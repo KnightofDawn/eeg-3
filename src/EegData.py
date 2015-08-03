54d334f181bc1ba18e2edff99ced0d764120fee1
neicullyn <neicullyn@gmail.com>
2015-08-02 22:15:19 -0700

    Change tab_size to 4 spaces

--- src/EegData.py
+++ src/EegData.py
@@ -2,78 +2,78 @@ import os
 import numpy as np
 
 class EegData:
-  TYPE_DATA = 'data'
-  TYPE_EVENTS = 'events'
+    TYPE_DATA = 'data'
+    TYPE_EVENTS = 'events'
 
-  def __init__(self, name=None, eeg_type=None, data=None):
-    self.name = name
-    self.type = eeg_type
-    self.data = data
+    def __init__(self, name=None, eeg_type=None, data=None):
+        self.name = name
+        self.type = eeg_type
+        self.data = data
 
-  def save(self, output_dir):
-    # Note that the file name should be generated from self.name
-    # Therefore the input parameter is output_dir
-    if self.type == self.TYPE_DATA:
-      output_path = os.path.join(output_dir, self.name + '_data.bin')
-    elif self.type == self.TYPE_EVENTS:
-      output_path = os.path.join(output_dir, self.name + '_events.bin')
-    else:
-      print 'Error: unknown eeg_type when saving'
-      return
+    def save(self, output_dir):
+        # Note that the file name should be generated from self.name
+        # Therefore the input parameter is output_dir
+        if self.type == self.TYPE_DATA:
+            output_path = os.path.join(output_dir, self.name + '_data.bin')
+        elif self.type == self.TYPE_EVENTS:
+            output_path = os.path.join(output_dir, self.name + '_events.bin')
+        else:
+            print 'Error: unknown eeg_type when saving'
+            return
 
-    with open(output_path, 'wb') as output:
-      np.save(output, self.data)
+        with open(output_path, 'wb') as output:
+            np.save(output, self.data)
 
-  def load(self, input_dir, input_filename):
-    # Note that we need to specify the file name
-    input_path = os.path.join(input_dir, input_filename)
+    def load(self, input_dir, input_filename):
+        # Note that we need to specify the file name
+        input_path = os.path.join(input_dir, input_filename)
 
-    r = input_filename.rfind('.bin')
-    input_filename = input_filename[0 : r]
+        r = input_filename.rfind('.bin')
+        input_filename = input_filename[0 : r]
 
-    r = input_filename.rfind('_')
-    self.name = input_filename[0 : r]
+        r = input_filename.rfind('_')
+        self.name = input_filename[0 : r]
 
-    if input_filename[r + 1 : ] == 'data':
-      self.type = self.TYPE_DATA
-    elif input_filename[r + 1 : ] == 'events':
-      self.type = self.TYPE_EVENTS
-    else:
-      print 'Error: unkonwn eeg_type when loading'
-      return
+        if input_filename[r + 1 : ] == 'data':
+            self.type = self.TYPE_DATA
+        elif input_filename[r + 1 : ] == 'events':
+            self.type = self.TYPE_EVENTS
+        else:
+            print 'Error: unkonwn eeg_type when loading'
+            return
 
-    self.data = np.load(input_path)
+        self.data = np.load(input_path)
 
-  def to_string(self):
-    return ''.join(
-      map(
-        lambda (i, row): '{}_{},{}\n'.format(
-          self.name, i, self.row_to_string(row)
-        ),
-        enumerate(self.data),
-      )
-    )
+    def to_string(self):
+        return ''.join(
+            map(
+                lambda (i, row): '{}_{},{}\n'.format(
+                    self.name, i, self.row_to_string(row)
+                ),
+                enumerate(self.data),
+            )
+        )
 
-  @staticmethod
-  def row_to_string(row):
-    return ','.join([str(x) for x in row])
+    @staticmethod
+    def row_to_string(row):
+        return ','.join([str(x) for x in row])
 
-  @staticmethod
-  def output_submission(prediction_array, output_path):
-    for prediction in prediction_array:
-      if prediction.type != EegData.TYPE_EVENTS:
-        print 'Error: prediction_array contains a invalid prediction'
-        return
+    @staticmethod
+    def output_submission(prediction_array, output_path):
+        for prediction in prediction_array:
+            if prediction.type != EegData.TYPE_EVENTS:
+                print 'Error: prediction_array contains a invalid prediction'
+                return
 
-    prediction_array.sort()
+        prediction_array.sort()
 
-    header = 'id,HandStart,FirstDigitTouch,BothStartLoadPhase,LiftOff,Replace,BothReleased\n'
+        header = 'id,HandStart,FirstDigitTouch,BothStartLoadPhase,LiftOff,Replace,BothReleased\n'
 
-    buf_arr = map(lambda x: x.to_string(), prediction_array)
-    content = header + ''.join(buf_arr)
+        buf_arr = map(lambda x: x.to_string(), prediction_array)
+        content = header + ''.join(buf_arr)
 
-    with open(output_path, 'w') as output_file:
-      output_file.write(content)
+        with open(output_path, 'w') as output_file:
+            output_file.write(content)
 
 # Test examples
 
